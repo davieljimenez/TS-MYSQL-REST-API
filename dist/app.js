2dde9f8af4c047c2110d2fd09f392c8524a5984e
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-14 19:01:23 -0400

    Convert code from TS to JS

--- dist/app.js
+++ dist/app.js
@@ -14,18 +14,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+// ROUTES
+const index_routes_1 = __importDefault(require("./routes/index.routes"));
+const post_routes_1 = __importDefault(require("./routes/post.routes"));
 class App {
     constructor(port) {
         this.port = port;
         this.app = express_1.default();
+        this.settings();
+        this.middlewares();
+        this.routes();
     }
     settings() {
         this.app.set("port", this.port || process.env.PORT || 8000);
     }
+    middlewares() {
+        this.app.use(morgan_1.default("dev"));
+        this.app.use(express_1.default.json());
+    }
+    routes() {
+        this.app.use(index_routes_1.default);
+        this.app.use("/posts", post_routes_1.default);
+    }
     listen() {
         return __awaiter(this, void 0, void 0, function* () {
-            yield this.app.listen();
-            console.log(`Server on port 8000`);
+            yield this.app.listen(this.app.get("port"));
+            console.log(`Server on port `, this.app.get("port"));
         });
     }
 }
---
+++ dist/controllers/index.controller.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IndexWelcome = void 0;
+function IndexWelcome(req, res) {
+    return res.json("Welcome to my API");
+}
+exports.IndexWelcome = IndexWelcome;
+;
